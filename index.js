ec895b0e95b968949879f510821a9aa4e33610ce
alassane-traore <alassanet076@gmail.com>
2022-08-21 23:32:09 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+let num1 = 31
+let num2 = 2
+const multiply = num1 * num2 
 
-// Write your code here
+let random = Math.round(Math.random())
+
+const num3 =  16
+const num4 = 6
+let mod = num3 % num4
+
+
+const max = Math.max (2,5,3,20,4,1)
+
